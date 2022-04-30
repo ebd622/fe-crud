3ab33e74f8cac51f7c2d7424aeb994ad0a284ef6
Eugeny <eburdo@gmail.com>
2022-04-30 12:15:24 +0200

    add disable/enable logic for buttons

--- crud-app/src/app/manage-product/manage-product.component.ts
+++ crud-app/src/app/manage-product/manage-product.component.ts
@@ -40,6 +40,7 @@ export class ManageProductComponent implements OnInit {
   }
   onUpdateProduct(name: string, desctiption: string, price: string, status: string) {
     this.productService.updateProduct({name: name, description: desctiption, price: price});
+    this.updateButtonDisable = true; //Disable [Update]-button after updating
     this.loggingService.logMessage('manage-component: onUpdateProduct(...)');
   }
 
