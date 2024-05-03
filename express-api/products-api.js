924feb1a1fbcdf671a526fb0e4cb4a0db78b1405
Eugeny <eburdo@gmail.com>
2024-05-04 00:08:06 +0200

    add express server

--- express-api/products-api.js
+++ express-api/products-api.js
@@ -2,8 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/v1/', (req, res) => {
+
+  const users = [
+    { id: 1, name: 'John Doe' },
+    { id: 2, name: 'Jane Smith' },
+    { id: 3, name: 'Bob Johnson' }
+  ];
+  //res.send('Hello World!')
+  //res.json({msg: 'Hello'})
+  res.json(users)
 })
 
 app.listen(port, () => {
