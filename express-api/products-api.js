f7a1ad46c41c5f29ab1322e56c7a8c796e05b620
Eugeny <eburdo@gmail.com>
2024-05-04 23:29:29 +0200

    add logic to express server

--- express-api/products-api.js
+++ express-api/products-api.js
@@ -2,17 +2,40 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/v1/', (req, res) => {
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-  const users = [
-    { id: 1, name: 'John Doe' },
-    { id: 2, name: 'Jane Smith' },
-    { id: 3, name: 'Bob Johnson' }
-  ];
+//Default list of products which supposed to be retrieved from BD
+const products = [
+  { id: 1, naam: 'Monitor', merk: `Samsung`, voorraad: 20, price:120.1},
+  { id: 2, naam: 'Monitor', merk: `Accer`, voorraad: 25, price:150.5},
+  { id: 3, naam: 'Mac', merk: `Apple`, voorraad: 15, price:1150.15},
+  { id: 4, naam: 'PC', merk: `Dell`, voorraad: 34, price:1000.25},
+];
+
+//--- HTTP GET: Fetch list of products
+ app.get('/v1/products', (req, res) => {
+  //Get all products from DB
+
   //res.send('Hello World!')
   //res.json({msg: 'Hello'})
-  res.json(users)
+  res.json(products)
 })
+
+//--- HTTP POST: Add a new product
+app.post('/v1/products', (req, res) => {
+  const product = req.body;
+
+  // Perform any necessary validation or processing
+
+  // Example response
+  const newProduct = {
+    id: products.length +1
+  };
+
+  products.push(product)
+  res.json(newProduct);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
